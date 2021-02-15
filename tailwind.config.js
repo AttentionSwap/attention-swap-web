478a4f1648be74763fbaa079c1a87c40ac9329d4
devnasim <devnasim18@gmail.com>
2021-02-16 03:35:46 +0530

    Added Font Family and Extended color example

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Nova Mono', 'monospace'],
+      serif: ['Nova Mono', 'monospace'],
+    },
+    extend: {
+      colors: {
+        primary: {
+          100: '#5F99F7',
+          150: '#5F99F7',
+          200: '#5F99F7',
+          250: '#5F99F7',
+          300: '#5F99F7',
+          350: '#5F99F7',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
